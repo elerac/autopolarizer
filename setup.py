7a6551c6c7500bd838af0179bc6241108663dac4
elerac <maenosuke079@gmail.com>
2021-08-31 16:17:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='autopolarizer',
-    version='1.0.0',
+    version='1.1.0',
     description='Control the automatic polarizer holder with Python',
     url='https://github.com/elerac/autopolarizer',
     author='Ryota Maeda',
     author_email='maeda.ryota.elerac@gmail.com',
     license='MIT',
+    install_requires=['pyserial'],
+    entry_points={'console_scripts': ['autopolarizer= autopolarizer.autopolarizer:main']},
     packages=find_packages()
 )
